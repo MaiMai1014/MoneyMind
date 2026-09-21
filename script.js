a54144a19bc292b4f152cd34facced5e73146481
MaiMai1014 <mairazafar1014@gmail.com>
2026-09-21 19:52:25 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,1 @@
+const menuBtn=document.querySelector('.menu-btn');const nav=document.querySelector('.nav nav');if(menuBtn&&nav){menuBtn.addEventListener('click',()=>nav.classList.toggle('open'));nav.querySelectorAll('a').forEach(a=>a.addEventListener('click',()=>nav.classList.remove('open')))}
